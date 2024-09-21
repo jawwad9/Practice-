8199af9ab24e669798ccc6d9fe9670705e63917c
jawwad9 <jawwadmuhammad514@gmail.com>
2024-09-21 19:51:13 +0500

    Complete

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Card from './components/Card'
 
-const App = () => {
+export const App = () => {
   return (
-    <div>App</div>
+<>
+<h1>Hello World</h1>
+<Card src = "https://media.wisemarket.com.pk/variant/SamsungGalaxyA555GBrandNewAwesomeLemon256GB8GBRAM-15.webp" price = "130999" title = "Samsung Galaxy A55 5G" />
+<Card src = "https://media.wisemarket.com.pk/variant/SamsungGalaxyA555GBrandNewAwesomeLemon256GB8GBRAM-15.webp" price = "130999" title = "Samsung Galaxy A55 5G" />
+
+</>
   )
 }
 
---
+++ props/src/components/Card.jsx
@@ -0,0 +1,29 @@
+
+// import React from 'react'
+
+// const Card = (props) => {
+//   return (
+// <>
+// <img src={props.src} alt="" />
+// <h1>PKR: {props.price}</h1>
+// <h2>{props.title}</h2>
+// </>  
+// )
+// }
+
+// export default Card
+
+
+import React from 'react'
+
+const Card = ({src, price, title}) => {
+  return (
+<>
+<img src={src} alt="" />
+<h1>PKR: {price}</h1>
+<h2>{title}</h2>
+</>  
+)
+}
+
+export default Card
